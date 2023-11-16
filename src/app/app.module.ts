e8c66e67eb993bbad40d3ecc532295f991d16faa
CarlosBarbosaFilho <cbarbosagomesfilho@gmail.com>
2023-11-15 22:01:42 -0300

    configure route to nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+//Components project
 import { NavComponent } from './components/nav/nav.component';
 
 
